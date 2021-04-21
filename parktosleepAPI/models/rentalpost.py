7a77e35a1b6f155c1e84be1e0afa3936e0ba68d8
Charles Butterworth <charlessbutterworth@gmail.com>
2021-04-21 15:21:09 -0500

    took out the time field, made it hard to read. Am using only date for bookings

--- parktosleepAPI/models/rentalpost.py
+++ parktosleepAPI/models/rentalpost.py
@@ -9,8 +9,8 @@ class RentalPost(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
-    start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
-    end_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_time = models.DateField(auto_now=False, auto_now_add=False)
+    end_time = models.DateField(auto_now=False, auto_now_add=False)
 
     @property
     def is_current_user(self):
